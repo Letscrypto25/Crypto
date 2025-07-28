53c67a383476014d022772fb477c1540ee6eab75
Letscrypto25 <letscrypto25@gmail.com>
2025-07-28 11:54:33 +0200

    Add files via upload

---
+++ migration/6/down.sql
@@ -0,0 +1,5 @@
+
+ALTER TABLE user_profiles DROP COLUMN luno_balance_zar;
+ALTER TABLE user_profiles DROP COLUMN luno_balance_btc;
+ALTER TABLE user_profiles DROP COLUMN luno_balance_eth;
+ALTER TABLE user_profiles DROP COLUMN luno_last_sync;
